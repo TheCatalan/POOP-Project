f75453f6c7c5bc1a636f0cb26e571a226a946ff1
TheCatalan <ryanghamandi1@gmail.com>
2020-03-26 18:14:05 -0400

    Created home component and added imports to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
