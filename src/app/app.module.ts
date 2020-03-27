9b6c78b1e41525bd056c8d7f27fda04e3dac71ac
TheCatalan <ryanghamandi1@gmail.com>
2020-03-26 22:22:45 -0400

    Added Login Component, Started To Implement It

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
