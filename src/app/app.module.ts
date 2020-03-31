7ac85dc6e0d6e5a923a2f5ac1a28432595a5a29b
TheCatalan <ryanghamandi1@gmail.com>
2020-03-30 23:38:53 -0400

    Experimenting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 
@@ -9,9 +10,11 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
